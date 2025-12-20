acecc0c88bf6727247e437893d7376c7e621e0ea
boardwalkclay1 <boardwalkclay1@gmail.com>
2025-12-20 14:27:51 -0500

    Update supabase.js

--- Laundry_Spot_app/supabase.js
+++ Laundry_Spot_app/supabase.js
@@ -3,5 +3,5 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const supabase = createClient(
     "https://iorpijdiswctyawndzna.supabase.co",
-    "YOUR_SUPABASE_ANON_KEY"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvcnBpamRpc3djdHlhd25kem5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyMDU0NTUsImV4cCI6MjA4MTc4MTQ1NX0.Wv0P82zsthA7vr5yw2thWwmHE8aG-lX5qku9iriA5rM"
 );

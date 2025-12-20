335ad8b17167096ffa48ae616f3ca4f8e647c3fa
boardwalkclay1 <boardwalkclay1@gmail.com>
2025-12-20 14:18:41 -0500

    Create supabase.js

---
+++ Laundry_Spot_app/supabase.js
@@ -0,0 +1,7 @@
+// supabase.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+    "https://iorpijdiswctyawndzna.supabase.co",
+    "YOUR_SUPABASE_ANON_KEY"
+);
